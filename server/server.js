fae74f7d4711586388d8917325954db581b0aab7
SediaM <sediamassaquoi@gmail.com>
2023-11-04 12:20:09 -0400

    apollo and graphql installed. Login file edited to have correct code.

--- server/server.js
+++ server/server.js
@@ -36,15 +36,14 @@ const startApolloServer = async () => {
   }
   app.use(routes);
 
-  // db.once('open', () => {
-  //   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  // });
   db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    });
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
   });
+  // db.once('open', () => {
+  //   app.listen(PORT, () => {
+  //     console.log(`API server running on port ${PORT}!`);
+  //     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  //   });
 };
 
 // Call the async function to start the server
